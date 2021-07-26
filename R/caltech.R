88ed195253f2947b41d5ffcd860f972864fdf608
Gregory Jefferis <jefferis@gmail.com>
2021-07-26 19:44:37 +0100

    Switch to rmatio - much faster!

--- R/caltech.R
+++ R/caltech.R
@@ -6,21 +6,22 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' '~/projects/Dana/tracking/samples/210517_006-feat.mat'
+#' x=read_caltech_feat('~/projects/Dana/tracking/samples/210517_006-feat.mat')
 #' }
 read_caltech_feat <- function(f) {
-  x=R.matlab::readMat(f)
+  x=rmatio::read.mat(path.expand(f))
   checkmate::assert_list(x, len=1)
   feat.names=unlist(x$feat[[1]], use.names = F)
   checkmate::assert_character(feat.names)
 
-  feat.units <- sapply(x$feat[[2]], function(y) {
+  feat.units=unlist(x$feat$units, recursive = F)
+  feat.units <- sapply(feat.units, function(y) {
     uy=unlist(y, use.names = F)
     ifelse(length(uy)==0, NA_character_ ,as.character(uy))
   })
 
   checkmate::assert_character(feat.units, len = length(feat.names))
-  feat.data=x$feat[[3]]
+  feat.data=x$feat$data[[1]]
   # [n_flies x n_frames x n_fields double] feature data matrix
   checkmate::assert_array(feat.data, d=3L)
   nflies=dim(feat.data)[1]
